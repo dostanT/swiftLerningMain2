08966248d87fd8d4983573f377f2edd72b22a272
Dostan <turlybekdostan@gmail.com>
2025-03-01 20:37:17 +0500

    Initial Commit

---
+++ swiftLerningMain2/swiftLerningMain2App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftLerningMain2App.swift
+//  swiftLerningMain2
+//
+//  Created by Dostan Turlybek on 01.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct swiftLerningMain2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
